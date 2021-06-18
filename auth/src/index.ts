4e997059bebe53c16cf0c3f927edf3c0123d239c
Yang Jace Liu <ly950710@gmail.com>
2021-06-18 15:24:54 -0600

    Use error validation
    Signed-off-by: Yang Jace Liu <ly950710@gmail.com>

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,8 @@ import {signupRouter} from "./routes/signup";
 import {signinRouter} from "./routes/signin";
 import {signoutRouter} from "./routes/signout";
 
+import {errorHandler} from "./middlwares/error-handler";
+
 const app = express();
 app.use(json())
 
@@ -13,6 +15,8 @@ app.use(currentUserRouter)
 app.use(signupRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
     console.log("Listening at 3000 port!!!!!!!!")
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,23 @@
 import express from 'express';
-import {body} from 'express-validator';
+import {body, validationResult} from 'express-validator';
 
 const router = express.Router()
 
-router.get('/api/users/signup', [
+router.post('/api/users/signup', [
     body('email').isEmail().withMessage("Email must be valid"),
     body('password').trim().isLength({min: 4, max: 20}).withMessage("Password must be between 4 and 20 characters")
 ], (req: express.Request, res: express.Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        throw new Error("Invalid email or password");
+    }
     const {email, password} = req.body;
+
+    console.log('Create a user ...');
+    throw new Error("Error connecting to dataset");
+
+    res.send({})
 });
 
 
